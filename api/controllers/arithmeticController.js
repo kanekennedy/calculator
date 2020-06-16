b59ae06a92d04cb061e94c080499203c1e9966b5
kanekennedy <38073629+kanekennedy@users.noreply.github.com>
2020-06-16 11:04:48 +0100

    az400 class

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b &*^%R },
+    'add':      function(a,b) { return a + b &*^%hgjjR },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
